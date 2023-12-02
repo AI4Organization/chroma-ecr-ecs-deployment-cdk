81d88f33c56d698194609666f29cc48381688388
KennyDizi <xamarindevelopervietnam@outlook.com>
2023-12-02 22:06:14 +0700

    Add resources

---
+++ lib/ChromaDockerImageEcrDeploymentCdkStackProps.ts
@@ -0,0 +1,8 @@
+import * as cdk from 'aws-cdk-lib';
+
+export interface ChromaDockerImageEcrDeploymentCdkStackProps extends cdk.StackProps {
+    readonly repositoryName: string;
+    readonly appName: string;
+    imageVersion?: string;
+    environment?: string;
+}
